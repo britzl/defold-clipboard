de6f1d7bc3cb8023a2eeca530d4649ade537e1bc
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-10-18 14:42:29 +0200

    Removed debug logs

--- clipboard/src/clipboard_ios.mm
+++ clipboard/src/clipboard_ios.mm
@@ -6,14 +6,12 @@
 #include <UIKit/UIKit.h>
 
 void clipboard_to_clipboard(const char* text) {
-	dmLogError("clipboard_to_clipboard %s", text);
 	UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
 	[pasteboard setPersistent:TRUE];
 	[pasteboard setString:[NSString stringWithUTF8String:text]];
 }
 
 const char* clipboard_from_clipboard() {
-	dmLogError("clipboard_from_clipboard");
 	UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
 	return [[pasteboard string] UTF8String];
 }
